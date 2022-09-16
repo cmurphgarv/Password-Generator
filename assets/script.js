30e984fd523c18258d516e08f1b7f5832712f555
Charlotte Murphey <murphey.charlotte@gmail.com>
2022-09-16 10:01:01 -0400

    added javascript for random password generator

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,26 @@
 // Assignment code here
+function generatePassword() {
+  // Create variables from each of the prompts required of user to make criteria for password
+  var passLengthMin = window.prompt("What is the minimum number of characters allowed in the password?");
+  var passLengthMax = window.prompt("What is the maximum number of characters allowed in the password?");
+  alert("All character types will be used for the password (uppercase, lowercase, numbers, and special characters)");
+  
+  // password will start out as an empty string
+  var password = "";
 
+  // all characters allowed to be in password
+  var chars = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%&*ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var passwordLength = Math.floor(Math.random() * passLengthMax + passLengthMin);
+
+  // use a for loop to generate password by iterating over passwordLength
+  for (var i = 0; i<= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+  }
+  return password;
+
+}
 
 
 // Get references to the #generate element
@@ -11,7 +32,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  console.log(password)
 }
 
 // Add event listener to generate button
